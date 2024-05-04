9b0066ad2e9c24d2cd786a99563f34e84e7c6a1c
Dikshant Chitara <93652656+dikshantchitara@users.noreply.github.com>
2024-05-04 17:16:25 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -204,13 +204,13 @@ const Home = () => {
           <Accordion.Item eventKey="2">
             <Accordion.Header>Accordion Item #3</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed dokjnflknafpionefgk
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Utkjnflknafpionefgk
+              enim ad minim veniam, quis nostrud exercitation ullamco laboriskjnflknafpionefgk
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor ikjnflknafpionefgkn
+              reprehenderit in voluptate velit esse cillum dolore eu fugiatkjnflknafpionefgk
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,kjnflknafpionefgk
+              sunt in culpa qui officia deserunt mollit anim id est laborum.kjnflknafpionefgk
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
